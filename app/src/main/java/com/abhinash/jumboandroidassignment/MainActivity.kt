bfa1d4e1ae521f6eddffe58c61eb88c734d4cc7a
Abhinash Pati <abhinash.pati@coolblue.nl>
2023-02-21 18:49:10 +0100

    Add MainActivity and Application

---
+++ app/src/main/java/com/abhinash/jumboandroidassignment/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.abhinash.jumboandroidassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity: AppCompatActivity() {
+}
